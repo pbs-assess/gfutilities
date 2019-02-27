1594e3e6eaa2c11cb4b5e2d011adfc3360a8774c
Chris Grandin <cgrandin@shaw.ca>
2019-02-27 13:22:38 -0800

    Add first two files.

---
+++ R/function-utils.R
@@ -0,0 +1,25 @@
+#' get.args
+#'
+#' @return a list of the argument values used in a function call
+#'
+#' @examples
+#' eg <- function(a = 1, b = 2, c = 5){
+#'   get.args()
+#' }
+#' eg()
+#' eg(10, c = 20)
+get.args <- function(){
+    def.call <- sys.call(-1)
+    def <- get(as.character(def.call[[1]]), mode="function", sys.frame(-2))
+    act.call <- match.call(definition = def, call = def.call)
+    def <- as.list(def)
+    def <- def[-length(def)]
+    act <- as.list(act.call)[-1]
+
+    def.nm <- names(def)
+    act.nm <- names(act)
+    inds <- def.nm %in% act.nm
+    out <- def
+    out[inds] <- act
+    out
+}
