556d6d3777b4b5c527158b52dd6886629cc41d33
Chris Grandin <chris.grandin@gmail.com>
2020-03-31 23:05:40 -0700

    Add pad.num()

--- R/strings.R
+++ R/strings.R
@@ -168,3 +168,15 @@ commify <- function(vec, use_and = TRUE, and_word="and"){
   }
   tmp
 }
+
+#' Pad the beginning of a number with zeroes
+#'
+#' @param num A vector of the numbers to pad
+#' @param digits The number of characters that the resulting strings should have
+#'
+#' @return A vector of strings of the padded numbers
+#' @export
+pad.num <- function(num, digits = 1){
+  stopifnot(digits >= 1, !any(nchar(num) > digits))
+  sapply(num, function(x){paste0(paste0(rep("0", digits - nchar(as.character(x))), collapse = ""), as.character(x))})
+}
