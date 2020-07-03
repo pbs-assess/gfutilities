35809ebb812a93c7c636bedecff689a6c2727486
Chris Grandin <chris.grandin@gmail.com>
2020-07-03 02:19:08 -0700

    Fix tests for group_quantiles()

--- R/quantiles.R
+++ R/quantiles.R
@@ -6,12 +6,11 @@
 #' calculations. Must be in `df` or an error will be thrown
 #' @param probs A vector of quantile probabilities to pass to [stats::quantile()]
 #' @param include_mean If TRUE, include the mean in the output as the column `avg`
-#' @param prepend_colname If TRUE, the name of the column used in the quantile calculation
-#' will be prepended to each quantile value column name
 #'
 #' @return A [data.frame] with a new column for each value in the `probs` vector
-#' @importFrom purrr set_names partial map
-#' @importFrom dplyr vars mutate summarize_at group_by group_map select everything
+#' @importFrom purrr set_names partial map map2
+#' @importFrom dplyr vars mutate summarize summarize_at group_by
+#' @importFrom dplyr group_map select everything ungroup bind_cols
 #' @importFrom rlang sym syms
 #' @importFrom stats quantile
 #' @export
@@ -26,13 +25,14 @@ group_quantiles <- function(df = NULL,
                             grp_cols = NULL,
                             col = NULL,
                             probs = c(0.05, 0.25, 0.5, 0.75, 0.95),
-                            include_mean = TRUE,
-                            prepend_colname = TRUE){
+                            include_mean = TRUE){
 
   verify_argument(df, "data.frame")
   verify_argument(col, "character", chk_is_in = names(df))
   verify_argument(grp_cols, "character", chk_is_in = names(df))
-  stopifnot(class(df[[col]]) == "numeric" | class(df[[col]]) == "integer")
+  map(col, ~{
+    stopifnot(class(df[[.x]]) == "numeric" | class(df[[.x]]) == "integer")
+  })
   verify_argument(probs, "numeric")
   if(any(probs <= 0)){
     stop("`probs` must all be positive values",
@@ -41,23 +41,25 @@ group_quantiles <- function(df = NULL,
   verify_argument(include_mean, "logical", 1)
 
   calc_helper <- function(df, col, probs, include_mean){
-    nms <- probs
-    if(prepend_colname){
-      nms <- paste0(col, "_", probs)
-    }
-    col_sym <- sym(col)
+    nms <- map(col, ~{
+      paste0(.x, "_", probs)
+    })
     # There is some explanation of summarize_at/map/partial here:
     # https://tbradley1013.github.io/2018/10/01/calculating-quantiles-for-groups-with-dplyr-summarize-and-purrr-partial/
-    out <- summarize_at(df,
-                        vars(!!col_sym),
-                        map(probs,
-                            ~partial(quantile, probs = .x, na.rm = TRUE)) %>%
-                          set_names(nms))
-    if(include_mean){
-      avg <- sym(ifelse(prepend_colname, paste0(col, "_avg"), "avg"))
-      out <- out %>%
-        mutate(!!avg := mean(df[[col]]))
-    }
+    out <- map2(col, nms, ~{
+      col_sym <- sym(.x)
+      tmp <- summarize_at(df,
+                          vars(!!col_sym),
+                          map(probs,
+                              ~partial(quantile, probs = .x, na.rm = TRUE))) %>%
+        set_names(.y)
+      if(include_mean){
+        avg <- sym(paste0(.x, "_avg"))
+        tmp <- tmp %>%
+          mutate(!!avg := mean(df[[.x]]))
+      }
+      tmp
+    }) %>% bind_cols
     out
   }
 
--- tests/testthat/test-quantiles.R
+++ tests/testthat/test-quantiles.R
@@ -1,7 +1,7 @@
 context("Test the group_quantiles() function")
 
 # Use a partial storms dataset
-df <- storms %>% filter(year %in% 2000:2005)
+df <- dplyr::storms %>% dplyr::filter(year %in% 2000:2005)
 
 probs <- c(0.05, 0.25, 0.5, 0.75, 0.95)
 
@@ -26,44 +26,52 @@ test_that("Arguments are acceptable", {
 test_that("Values are correct", {
   grp <- group_quantiles(df, grp_col = c("year", "month", "day"), col = "wind", probs = probs)
 
-  actual <- df %>% filter(year == 2001, month == 6, day == 8)
+  actual <- df %>% dplyr::filter(year == 2001, month == 6, day == 8)
   actual_q <- quantile(actual$wind, probs)
   attributes(actual_q)$names <- NULL
   grp_q <- grp %>%
-    filter(year == 2001, month == 6, day == 8) %>%
-    select(-c(year, month, day, wind_avg)) %>%
-    slice(1) %>%
+    dplyr::filter(year == 2001, month == 6, day == 8) %>%
+    dplyr::select(-c(year, month, day, wind_avg)) %>%
+    dplyr::slice(1) %>%
     unlist(use.names = FALSE)
   expect_equal(grp_q, actual_q)
 
-  actual <- df %>% filter(year == 2005, month == 7, day == 12)
+  actual <- df %>% dplyr::filter(year == 2005, month == 7, day == 12)
   actual_q <- quantile(actual$wind, probs)
   attributes(actual_q)$names <- NULL
   grp_q <- grp %>%
-    filter(year == 2005, month == 7, day == 12) %>%
-    select(-c(year, month, day, wind_avg)) %>%
-    slice(1) %>%
+    dplyr::filter(year == 2005, month == 7, day == 12) %>%
+    dplyr::select(-c(year, month, day, wind_avg)) %>%
+    dplyr::slice(1) %>%
     unlist(use.names = FALSE)
   expect_equal(grp_q, actual_q)
 
-  grp <- group_quantiles(df, grp_col = "name", col = "wind", probs = probs)
-  actual <- df %>% filter(name == "Emily")
-  actual_q <- quantile(actual$wind, probs)
+  grp <- group_quantiles(df,
+                         grp_col = "name",
+                         col = c("wind", "pressure"),
+                         probs = probs,
+                         include_mean = FALSE)
+  actual <- df %>% dplyr::filter(name == "Emily")
+  actual_wind_q <- quantile(actual$wind, probs)
+  actual_pressure_q <- quantile(actual$pressure, probs)
+  actual_q <- c(actual_wind_q, actual_pressure_q)
   attributes(actual_q)$names <- NULL
   grp_q <- grp %>%
-    filter(name == "Emily") %>%
-    select(-c(name, wind_avg)) %>%
-    slice(1) %>%
+    dplyr::filter(name == "Emily") %>%
+    dplyr::select(-c(name)) %>%
+    dplyr::slice(1) %>%
     unlist(use.names = FALSE)
   expect_equal(grp_q, actual_q)
 
-  actual <- df %>% filter(name == "Claudette")
-  actual_q <- quantile(actual$wind, probs)
+  actual <- df %>% dplyr::filter(name == "Claudette")
+  actual_wind_q <- quantile(actual$wind, probs)
+  actual_pressure_q <- quantile(actual$pressure, probs)
+  actual_q <- c(actual_wind_q, actual_pressure_q)
   attributes(actual_q)$names <- NULL
   grp_q <- grp %>%
-    filter(name == "Claudette") %>%
-    select(-c(name, wind_avg)) %>%
-    slice(1) %>%
+    dplyr::filter(name == "Claudette") %>%
+    dplyr::select(-c(name)) %>%
+    dplyr::slice(1) %>%
     unlist(use.names = FALSE)
   expect_equal(grp_q, actual_q)
 
