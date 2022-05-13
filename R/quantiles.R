7ec813a3a41f8b95d18bc5973b10ad7cf0a5bc39
Chris Grandin <chris.grandin@gmail.com>
2022-05-13 15:11:32 -0700

    Add Rmarkdown Roxygen flag * Change group_quantiles to summarize_quants() and add docs for it

--- R/quantiles.R
+++ R/quantiles.R
@@ -1,59 +1,55 @@
-#' Calculate arbitrary quantiles for a particular column by a grouping
+#' Calculate arbitrary quantiles for particular column(s) by group
+#'
+#' @description
+#' Calculate arbitrary quantiles for particular column(s) by group
+#' and create a summary [data.frame] which holds a column for each
+#' unique combination of values of columns (`cols`) and quantile
+#' values (`probs`).
+#'
+#' @details
+#' Create a new [data.frame] which summarizes the one passed as `df`.
+#' The summary [data.frame] will have a column for each grouping column
+#' (`grp_cols`) and a set of columns for each column name (`cols`), where
+#' the set of columns is one for each value of `probs`. New columns will
+#' be compose of the name in `cols`, an underscore, and the value in `probs`.
 #'
 #' @param df A [data.frame]
-#' @param grp_cols A character vector of the column names to use for grouping the data
-#' @param cols A character vector representing column names on which to perform the
-#' calculations. They must all be in `df` or an error will be thrown
-#' @param probs A vector of quantile probabilities to pass to [stats::quantile()]
-#' @param include_mean If TRUE, include the mean in the output as the column `avg`
+#' @param grp_cols A character vector of the column names to use for grouping
+#' the data
+#' @param cols A character vector representing column names on which to perform
+#' the calculations
+#' @param probs A vector of quantile probabilities to pass to [stats::quantile]
+#' @param include_mean If [TRUE], include the mean in the output as the column
+#' ending in `*_avg`
 #'
-#' @return A [data.frame] with a new column for each value in the `probs` vector
-#' @importFrom purrr set_names partial map map2
-#' @importFrom dplyr vars mutate summarize summarize_at group_by
-#' @importFrom dplyr group_map select everything ungroup bind_cols
-#' @importFrom rlang sym syms
-#' @importFrom stats quantile
-#' @export
-#' @examples
-#' library(rlang)
-#' library(dplyr)
-#' library(purrr)
-#' df <- storms %>% filter(year %in% 2000:2005)
-#' probs <- c(0.05, 0.25, 0.5, 0.75, 0.95)
-#' q <- group_quantiles(df, grp_cols = c("year", "month", "hour"),
-#'  cols = c("wind", "pressure"), probs = probs)
-#' print(head(q, 5))
-group_quantiles <- function(df = NULL,
-                            grp_cols = NULL,
-                            cols = NULL,
-                            probs = c(0.05, 0.25, 0.5, 0.75, 0.95),
-                            include_mean = TRUE){
+#' @return A [data.frame] with a new column for each value in the `probs`
+#' vector and a column for the mean in `include_mean` is `TRUE`
+summarize_quants <- function(df = NULL,
+                             grp_cols = NULL,
+                             cols = NULL,
+                             probs = c(0.05, 0.25, 0.5, 0.75, 0.95),
+                             include_mean = TRUE){
 
-  verify_argument(df, "data.frame")
-  verify_argument(cols, "character", chk_is_in = names(df))
-  verify_argument(grp_cols, "character", chk_is_in = names(df))
-  map(cols, ~{
-    stopifnot(class(df[[.x]]) == "numeric" | class(df[[.x]]) == "integer")
-  })
-  verify_argument(probs, "numeric")
-  if(any(probs <= 0)){
-    stop("`probs` must all be positive values",
-         call. = FALSE)
-  }
-  verify_argument(include_mean, "logical", 1)
-
-  calc_helper <- function(df, cols, probs, include_mean){
+  # Perform [stats::quantile] calculations and add a column for each
+  # quantile value in `probs`
+  #
+  # @inheritParams summarize_quants
+  # @return A [data.frame] which has a new column for each value of
+  calc_helper <- function(df,
+                          cols,
+                          probs,
+                          include_mean){
     nms <- map(cols, ~{
       paste0(.x, "_", probs)
     })
-    # There is some explanation of summarize_at/map/partial here:
-    # https://tbradley1013.github.io/2018/10/01/calculating-quantiles-for-groups-with-dplyr-summarize-and-purrr-partial/
     out <- map2(cols, nms, ~{
       col_sym <- sym(.x)
       tmp <- summarize_at(df,
                           vars(!!col_sym),
                           map(probs,
-                              ~partial(quantile, probs = .x, na.rm = TRUE))) %>%
+                              ~partial(quantile,
+                                       probs = .x,
+                                       na.rm = TRUE))) %>%
         set_names(.y)
       if(include_mean){
         avg <- sym(paste0(.x, "_avg"))
@@ -61,18 +57,20 @@ group_quantiles <- function(df = NULL,
           mutate(!!avg := mean(df[[.x]]))
       }
       tmp
-    }) %>% bind_cols
+    }) %>%
+      bind_cols
     out
   }
 
   grp_cols_sym <- syms(grp_cols)
-  # Create a data frame of the grouped columns only, which are bound back on after the
-  # quantile columns are calculated
+  # Create a data frame of the grouped columns only, which are bound back
+  # on after the quantile columns are calculated
   grp_cols_df <- df %>%
     group_by(!!!grp_cols_sym) %>%
     summarize(tmp = 1) %>%
     ungroup() %>%
     select(-tmp)
+
   df %>%
     group_by(!!!grp_cols_sym) %>%
     group_map(~calc_helper(.x, cols, probs, include_mean)) %>%
